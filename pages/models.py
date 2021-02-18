f9c7544264578450b7dbf53f1527e452ebf77daa
ankurhacked1986 <ankurhacked@gmail.com>
2021-02-18 05:19:30 +0000

    Commit here

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    def __str__(self):
+        return self.first_name
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    image = models.ImageField(upload_to = 'pictures/')
+    twitter_link = models.URLField(max_length=200)
